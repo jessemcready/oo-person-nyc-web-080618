e3612f14d26522dcabd9aa1b118f177eac9985f3
jessemcready <jessemcready@gmail.com>
2018-08-07 17:19:23 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,88 @@
 # your code goes here
+require 'pry'
+
+class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+
+  def initialize(name, bank_account=25, happiness=8, hygiene=8)
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+
+  def happiness=(points)
+    @happiness = check_range(points)
+  end
+
+  def hygiene=(points)
+    @hygiene = check_range(points)
+  end
+
+  def check_range(points)
+    if points > 10
+      points = 10
+    elsif points < 0
+      points = 0
+    end
+
+    return points
+  end
+
+
+
+  def clean?
+    if @hygiene > 7
+      true
+    else
+      false
+    end
+  end
+
+  def happy?
+    if @happiness > 7
+      true
+    else
+      false
+    end
+  end
+
+  def get_paid(salary_amount)
+    @bank_account += salary_amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    case topic
+    when "politics"
+      friend.happiness -= 2
+      self.happiness -= 2
+      return "blah blah partisan blah lobbyist"
+    when "weather"
+      friend.happiness += 1
+      self.happiness += 1
+      return "blah blah sun blah rain"
+    else
+      return "blah blah blah blah blah"
+    end
+  end
+
+end
